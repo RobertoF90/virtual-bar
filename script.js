e080f8effd10d35459709b4dc60017a761b668b8
Roberto Franco <92464287+RobertoF90@users.noreply.github.com>
2023-01-24 10:27:18 +0100

    improved layout added icons

--- script.js
+++ script.js
@@ -18,44 +18,52 @@ const generateMarkup = () => {
   cards.forEach((c) => {
     let stars = [];
     if (c.valutazione.split("").length > 1) {
-      stars.push("half");
+      stars.push("<i class='fa-solid fa-star-half'></i>");
       for (let i = 0; i < c.valutazione - 1; i++) {
-        stars.unshift("⭐");
+        stars.unshift("<i class='fa-solid fa-star'></i>");
       }
     } else {
       for (let i = 0; i < c.valutazione; i++) {
-        stars.unshift("⭐");
+        stars.unshift("<i class='fa-solid fa-star'></i>");
       }
     }
 
+    let tipi = {
+      Calcio: '<i class="fa-regular fa-futbol"></i>',
+      Cani: '<i class="fa-solid fa-dog"></i>',
+      CaniReali: '<i class="fa-solid fa-dog"></i>',
+      Cavalli: '<i class="fa-solid fa-horse"></i>',
+    };
+
     const markup = `
-        <div class="card">
-                <div>
-                  <picture><img src="" alt="Logo" /></picture>
-                  <div>
+        <div class="card card--comparazioni">
+                <div class="card__container">
+                  <picture><img src="${c.logo}" alt="Logo" /></picture>
                     <h3>${c.website}</h3>
                     <div>${stars.join("")}</div>
-                  </div>
                 </div>
-                <div>
-                  <ul>
+                  <ul class="card__container">
                     ${
                       c.tipi
-                        ? c.tipi.map((t) => `<li>icona ${t}</li>`).join("")
+                        ? c.tipi
+                            .map(
+                              (t) =>
+                                `<li class="card__element"> ${tipi[t]} ${t}</li>`
+                            )
+                            .join("")
                         : "<li></li>"
                     }
              
                   </ul>
-                </div>
-                <div>
-                  <ul>
+                  <ul class="card__container">
                     <p>BONUS BENVENUTO</p>
                     <h4>${c.bonus}</h4>
                     <div>${c.bonusExtra}</div>
                   </ul>
-                </div>
-                <button class="btn card--btn">Visita Sito</button>
-              </div>
+                <a href="${c.link}" target="_blank">
+                   <button class="btn card--btn">Visita Sito</button>
+                </a>
+             </div>
         `;
 
     cardContainer.insertAdjacentHTML("afterbegin", markup);
